596fa3ab11554d8ac1fe2fb7cbea609c462a8bde
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-05-10 16:50:55 +0300

    Initial commit

---
+++ src/main/java/com/akbulutmehmet/enocaassignment/EnocaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.akbulutmehmet.enocaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akbulutmehmet/enocaassignment/EnocaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akbulutmehmet.enocaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
